df6dc4372c98d92902dc0929638c313cf62d8598
penielcantoral <48923974+penielcantoral@users.noreply.github.com>
2019-08-06 10:35:56 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+$scope.juego=function (){
+	$scope.personaje-();
+	var juego = $scope
+	$scope.personaje.push({
+		$scope.miembros.length + 1
+	})
+}
+/run BNSetMatureLanguageFilter(true)
+/run local x=GetAccountExpansionLevel(); SendChatMessage(x)
